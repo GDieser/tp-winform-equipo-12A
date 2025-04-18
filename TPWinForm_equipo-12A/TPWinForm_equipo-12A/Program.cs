160567c156ac102284e4587beaab98f0e1a185b9
Walter Ariel Lugo <walu197103@gmail.com>
2025-04-18 18:21:09 -0300

    pruebaWalter

--- TPWinForm_equipo-12A/TPWinForm_equipo-12A/Program.cs
+++ TPWinForm_equipo-12A/TPWinForm_equipo-12A/Program.cs
@@ -11,6 +11,7 @@ namespace TPWinForm_equipo_12A
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// prueda git walter
         [STAThread]
         static void Main()
         {
